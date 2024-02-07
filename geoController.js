3b17c70a76c46a83d5447d96b79f63be4aa221b0
priya-patra <priya.patra@thoughtspheres.com>
2024-02-07 17:50:42 +0530

    match the area from candidate array

--- geoController.js
+++ geoController.js
@@ -1,12 +1,12 @@
-const Service = require('./service')
+// const Service = require('./service')
 const { ApiKeyManager } = require('@esri/arcgis-rest-request')
 const { geocode } = require('@esri/arcgis-rest-geocoding')
-const apiKey = "AAPK9e4dae44522b4866b5d8708ac5d8b5bbZhc5tV_7NFDOsxNB8GS_XaBb3o9cdDkDhIrWGZq06nodYNnG0sDRX8pDPuqRV8pd";
+const apiKey = "AAPK9e4dae44522b4866b5d8708ac5d8b5bbZhc5tV_7NFDOsxNB8GS_XaBb3o9cdDkDhIrWGZq06nodYNnG0sDRX8pDPuqRV8pd"
 
 
 exports.getData = async (req, res) => {
     try {
-        const authentication = ApiKeyManager.fromKey(apiKey);
+        const authentication = ApiKeyManager.fromKey(apiKey)
         var location = {}
         var result = await geocode({
             address: req.body.address,
@@ -17,18 +17,31 @@ exports.getData = async (req, res) => {
             authentication,
         })
         console.log(result.candidates)
-        if (result.candidates[0] != undefined) {
-            var latitude = result.candidates[0].location.y.toString()
-            var longitude = result.candidates[0].location.x.toString()
-            location.latitude = latitude
-            location.longitude = longitude
-        } else {
-            location.latitude = '0'
-            location.longitude = '0'
+        if (result.candidates && result.candidates.length > 0) {
+            const inputArea = req.body.area.toLowerCase()
+
+            for (var candidate of result.candidates) {
+                const candidateArea = candidate.address.toLowerCase()
+                if (candidateArea.includes(inputArea)) {
+                    location.latitude = candidate.location.y.toString()
+                    location.longitude = candidate.location.x.toString()
+                    break 
+                }
+            }
         }
-        return res.status(200).send({data:location});
+        
+        // if (result.candidates[0] != undefined) {
+        //     var latitude = result.candidates[0].location.y.toString()
+        //     var longitude = result.candidates[0].location.x.toString()
+        //     location.latitude = latitude
+        //     location.longitude = longitude
+        // } else {
+        //     location.latitude = '0'
+        //     location.longitude = '0'
+        // }
+        return res.status(200).send({data:location})
     } catch (error) {
         console.log(error)
-        return res.status(500).send(error.message);
+        return res.status(500).send(error.message)
     }
 }
